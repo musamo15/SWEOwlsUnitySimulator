baf944d424dc8aae04d3a6f65b7160de02015486
Rileyz86 <91497785+Rileyz86@users.noreply.github.com>
2021-11-29 13:12:28 -0500

    added user level and changed load style for level

--- Assets/Unity Save Load Utility/LoadMenu.cs
+++ Assets/Unity Save Load Utility/LoadMenu.cs
@@ -8,13 +8,12 @@ public class LoadMenu : MonoBehaviour
 {
 
 	public SaveLoadUtility slu;
-	public bool showMenu = true;
-	//public bool showSave = false;
-	public bool showLoad = false;
+	public bool showLoad = true;
 	private string saveGameName;
 	private int selectedSaveGameIndex = -99;
 	public List<SaveGame> saveGames;
 	private char[] newLine = "\n\r".ToCharArray();
+	public Vector2 scrollPosition = Vector2.zero;
 
 	private Regex regularExpression = new Regex("^[a-zA-Z0-9_\"  *\"]*$");
 	/*Regular expression, contains only upper and lowercase letters, numbers, and underscores.
@@ -42,260 +41,37 @@ public class LoadMenu : MonoBehaviour
 				Debug.Log("[SaveLoadMenu] Start(): Warning! SaveLoadUtility not assigned!");
 			}
 		}
+
+		saveGames = SaveLoad.GetSaveGames(slu.saveGamePath, slu.usePersistentDataPath);
+
 	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
-
-			if(showLoad == true) {
-				showLoad = false;
-				showMenu = true;
-				return;
-			}
-
-			//if (showSave == true)
-			//{
-			//	if (selectedSaveGameIndex != -99)
-			//	{
-			//		selectedSaveGameIndex = -99;
-			//		Debug.Log("sdf");
-			//	}
-			//	else
-			//	{
-			//		showSave = false;
-			//		showMenu = true;
-			//	}
-			//	return;
-			//}
-
-			if (showMenu == true)
-			{
-				showMenu = false;
-				return;
-			}
-			else
-			{
-				if (showLoad == false)
-				{ // showLoad == false || showSave == false
-					showMenu = true;
-					return;
-				}
-			}
-		}
-
-
-
-		//The classic hotkeys for quicksaving and quickloading
-		//if (Input.GetKeyDown(KeyCode.F5))
-		//{
-		//	slu.SaveGame(slu.quickSaveName);//Use this for quicksaving, which is basically just using a constant savegame name.
-		//}
-
-		if(Input.GetKeyDown(KeyCode.F9)) {
-			slu.LoadGame(slu.quickSaveName);//Use this for quickloading, which is basically just using a constant savegame name.
-
-		}
-	}
-
 
 	void OnGUI()
 	{
 
-
-
-
-		if (showMenu == false && showLoad == false)
-		{ // && showSave == false
-			if (GUILayout.Button("Menu"))
-			{
-				showMenu = true;
-				return;
-			}
-		}
-
-		if (showMenu == true)
-		{
-			GUILayout.BeginVertical(GUILayout.MinWidth(300));
-			GUILayout.BeginArea(new Rect((Screen.width / 2) - 50, (Screen.height / 2) - 50, 300, 300));
-
-			//if (GUILayout.Button("Save"))
-			//{
-			//	showMenu = false;
-			//	//showLoad = false;
-			//	saveGames = SaveLoad.GetSaveGames(slu.saveGamePath, slu.usePersistentDataPath);
-			//	showSave = true;
-			//
-			//	return;
-			//}
-
-			if(GUILayout.Button("Load")) {
-				//showSave = false;
-				showMenu = false;
-				saveGames = SaveLoad.GetSaveGames(slu.saveGamePath, slu.usePersistentDataPath);
-				if(saveGames.Count >= 0) {
-					showLoad = true;
-				}
-				else {
-					showMenu = true;
-				}
-				return;
-			}
-
-			if (GUILayout.Button("Clear"))
-			{
-				//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-				slu.ClearScene();
-				return;
-			}
-
-			if (GUILayout.Button("Close"))
-			{
-				//showSave = false;
-				showMenu = false;
-				showLoad = false;
-				return;
-			}
-
-			if (GUILayout.Button("Exit to Windows"))
-			{
-				Application.Quit();
-				return;
-			}
-
-			GUILayout.FlexibleSpace();
-			GUILayout.EndVertical();
-			GUILayout.EndArea();
-		}
 		if(showLoad == true) {
+			scrollPosition = GUI.BeginScrollView(new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 50, 320, 150), scrollPosition, new Rect(0, 0, 220, 500));
 			GUILayout.BeginVertical(GUILayout.MinWidth(300)); 
-			GUILayout.BeginArea(new Rect((Screen.width / 2) - 50, (Screen.height / 2) - 50, 300, 300));
+			GUILayout.BeginArea(new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 50, 300, 500));
 			foreach (SaveGame saveGame in saveGames) {
 				if(GUILayout.Button(saveGame.savegameName + " (" + saveGame.saveDate + ")")) {
 					slu.LoadGame(saveGame.savegameName);
 					showLoad = false;
+					GUI.EndScrollView();
 					return;
 				}
 			}
 
-			GUILayout.BeginHorizontal();
-			GUILayout.FlexibleSpace();
-			if(GUILayout.Button("Back", GUILayout.MaxWidth(100))) {
-				showLoad = false;
-				showMenu = true;
-			}
-			GUILayout.FlexibleSpace();
-			GUILayout.EndHorizontal();
+			//GUILayout.BeginHorizontal();
+			//GUILayout.FlexibleSpace();
+			//GUILayout.EndHorizontal();
 
 			GUILayout.FlexibleSpace();
-			GUILayout.EndArea();
-			GUILayout.EndVertical();
+			//GUILayout.EndArea();
+			//GUILayout.EndVertical();
+			//GUI.EndScrollView();
 
 		}
-		//if (showSave == true)
-		//{
-
-		//	GUILayout.BeginVertical(GUILayout.MinWidth(550));
-		//	GUILayout.BeginArea(new Rect((Screen.width / 2) - 50, (Screen.height / 2) - 50, 250, 200));
-		//	for (int i = -1; i < saveGames.Count; i++)
-		//	{
-
-		//		if (i == selectedSaveGameIndex)
-		//		{
-
-		//			GUILayout.BeginHorizontal(GUILayout.MinWidth(100));
-		//
-		//			string str = GUILayout.TextField(saveGameName, GUILayout.MinWidth(100));
-
-		//			if (regularExpression.IsMatch(str))
-		//			{
-		//				if (str.IndexOfAny(newLine) != -1)
-		//				{
-		//					//New Line detected
-		//					if (i >= 0)
-		//					{
-		//						SaveLoad.DeleteFile(slu.saveGamePath, saveGames[i].savegameName);
-		//					}
-		//					slu.SaveGame(saveGameName);
-		//					selectedSaveGameIndex = -99;
-		//					return;
-		//				}
-		//				else
-		//				{
-		//					saveGameName = str; //All OK, copy
-		//				}
-		//			}
-		//			else
-		//			{
-		//				Debug.Log("Irregular expression detected");
-		//			}
-
-		//			GUILayout.FlexibleSpace();
-		//			if (GUILayout.Button("Save", GUILayout.MaxWidth(50)))
-		//			{
-		//				if (i >= 0)
-		//				{
-		//					SaveLoad.DeleteFile(slu.saveGamePath, saveGames[i].savegameName);
-		//				}
-		//				slu.SaveGame(saveGameName);
-		//				selectedSaveGameIndex = -99;
-		//				saveGames = SaveLoad.GetSaveGames(slu.saveGamePath, slu.usePersistentDataPath);
-		//				return;
-		//			}
-
-		//			if (GUILayout.Button("Cancel", GUILayout.MaxWidth(50)))
-		//			{
-		//				selectedSaveGameIndex = -99;
-		//				return;
-		//			}
-		//			GUILayout.EndHorizontal();
-		//		}
-		//		else
-		//		{
-		//			if (i == -1)
-		//			{
-		//				if (GUILayout.Button("(New)"))
-		//				{
-		//					selectedSaveGameIndex = i;
-		//					saveGameName = "";
-		//					return;
-		//				}
-		//			}
-		//			else
-		//			{
-		//				if (GUILayout.Button(saveGames[i].savegameName + " (" + saveGames[i].saveDate + ")"))
-		//				{
-		//					selectedSaveGameIndex = i;
-		//					saveGameName = saveGames[i].savegameName;
-		//					return;
-		//				}
-		//			}
-		//		}
-		//	}
-
-		//	GUILayout.BeginHorizontal();
-		//	GUILayout.FlexibleSpace();
-		//	if (GUILayout.Button("Back", GUILayout.MaxWidth(100)))
-		//	{
-		//		if (selectedSaveGameIndex != -99)
-		//		{
-		//			selectedSaveGameIndex = -99;
-		//		}
-		//		else
-		//		{
-		//			showSave = false;
-		//			showMenu = true;
-		//		}
-		//	}
-		//	GUILayout.FlexibleSpace();
-		//	GUILayout.EndHorizontal();
-
-		//	GUILayout.FlexibleSpace();
-		//	GUILayout.EndVertical();
-		//	GUILayout.EndArea();
-
-		//}
+		
 	}
 }
